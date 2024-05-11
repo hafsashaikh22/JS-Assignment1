9596be8972a93d35e0e2eee6624b939720f74950
Hafsa shaikh <158846079+hafsashaikh22@users.noreply.github.com>
2024-05-11 13:49:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+alert("Error! please enter a valid password");
+ alert("welcome to js land\n happy coding ");
+ alert("welcome to js land...");
+ alert("happy coding!");
+ alert("Hello i can run JS through my web browser's console");
+ 
+
+ var userName;
+var myName = 'Hafsa Shaikh' 
+
+
+message = "Hello World";
+alert(message);
+
+
+var studentName = "John Doe";
+var studentAge = "15 years old";
+var studentProfession = "Certified Mobile Application Development";
+
+
+alert(studentName);
+alert(studentAge);
+alert(studentProfession);
+
+// Using Loop
+var word = "pizza";
+for (var i = word.length; i > 0; i--) {
+    alert(word.substring(0, i));
+}
+
+// Concatenation
+var email = "ShaikhZadi403@gmail.com";
+alert("My Email Address id : " + email);
+
+// Template Literals Or Back Ticks
+var book = "A smarter way to learn JavaScript";
+alert(`I am trying to learn from the Book ${book}`);
+
+//  write a message directly to the HTML document
+document.write("Yah! I can write HTML content through JavaScript")
+
+var age = 19;
+alert(`I am ${age} years old`);
+
+document.write("Variable names can only contain letters, numbers, underscores, and dollar signs. For example: $my_1stVariable<br>");
+document.write("Variables must begin with a letter, underscore, or dollar sign. For example: $name, _name, or name<br>");
+document.write("Variable names are case-sensitive.<br>");
+document.write("Variable names should not be JavaScript keywords.<br>");
